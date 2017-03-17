de9758f4ea42c3cf8e2cb9d00479b1f5f671cfb5
Gabriel Schulhof <gabriel.schulhof@intel.com>
2017-03-17 15:59:57 +0200

    Add project that uses it all

---
+++ parent/index.js
@@ -0,0 +1,5 @@
+var v1 = require( "uses-v1" );
+console.log( "v1 keys: " + JSON.stringify( Object.keys( v1 ) ) );
+
+var v2 = require( "uses-v2" );
+console.log( "v2 keys: " + JSON.stringify( Object.keys( v2 ) ) );
